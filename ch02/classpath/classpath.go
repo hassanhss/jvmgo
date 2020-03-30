a62b10fd10ab7f8147983d2258fa3c97202d60e3
hanshunshun <hanshunshun@pinming.cn>
2020-03-30 18:49:04 +0800

    classpath

--- ch02/classpath/classpath.go
+++ ch02/classpath/classpath.go
@@ -1,1 +1,33 @@
 package classpath
+
+import "os"
+
+type Classpath struct {
+	bootClasspath string
+	extClasspath  string
+	userClasspath string
+}
+
+func Parse(jreOption, cpOption string) *Classpath {
+	cp := &Classpath{}
+
+	return nil
+}
+
+func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
+	//jreDir := getJreDir(jreOption)
+}
+
+func getJreDir(jreOption string) string {
+	if jreOption != "" && exists(jreOption) {
+
+	}
+}
+
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+}
