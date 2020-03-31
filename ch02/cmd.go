5eabbb695180d9a4dfc9899ccf2b1f3d55d20988
hanshunshun <hanshunshun@pinming.cn>
2020-03-31 10:14:07 +0800

    main

--- ch02/cmd.go
+++ ch02/cmd.go
@@ -10,6 +10,7 @@ type Cmd struct {
 	helpFlag    bool
 	versionFlag bool
 	cpOption    string
+	XjreOption  string
 	class       string
 	args        []string
 }
@@ -38,8 +39,15 @@ func parseCmd() *Cmd {
 	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
 	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
 	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
-	flag.StringVar(&cmd.cpOption,"cp","","classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.StringVar(&cmd.XjreOption, "Xjre", "", "path to jre")
 	flag.Parse()
+
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
 
 	return cmd
 }
