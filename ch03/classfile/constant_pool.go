331cdaa36cecd04836000190b411be4a1cd29e90
hanshunshun <hanshunshun@pinming.cn>
2020-03-31 19:55:05 +0800

    ch03

---
+++ ch03/classfile/constant_pool.go
@@ -0,0 +1,26 @@
+package classfile
+
+import "fmt"
+
+type ConstantPool []ConstantInfo
+
+func readConstantPool(reader *ClassReader) ConstantPool {
+	cpCount := int(reader.readUint16())
+	cp := make([]ConstantInfo, cpCount)
+
+	for i := 1; i < cpCount; i++ {
+		cp[i] = readConstantInfo(reader, cp)
+		switch cp[i].(type) {
+		case *ConstantLongInfo, *ConstantDoubleInfo:
+			i++
+		}
+	}
+	return cp
+}
+
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic(fmt.Errorf("Invalid constant pool index:%v!", index))
+}
---
+++ ch03/classfile/member_info.go
@@ -0,0 +1,54 @@
+package classfile
+
+/*
+field_info {
+    u2             access_flags;
+    u2             name_index;
+    u2             descriptor_index;
+    u2             attributes_count;
+    attribute_info attributes[attributes_count];
+}
+method_info {
+    u2             access_flags;
+    u2             name_index;
+    u2             descriptor_index;
+    u2             attributes_count;
+    attribute_info attributes[attributes_count];
+}
+*/
+
+type MemberInfo struct {
+	cp              ConstantPool
+	accessFlag      uint16
+	nameIndex       uint16
+	descriptorIndex uint16
+	attributes      []AttributeInfo
+}
+
+func readMembers(reader *ClassReader, cp ConstantPool) []*MemberInfo {
+	memberCount := reader.readUint16()
+	//创建membercount个MemberInfo
+	members := make([]*MemberInfo, memberCount)
+	for i := range members {
+		members[i] = readMember(reader, cp)
+	}
+	return members
+}
+
+func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo {
+	return &MemberInfo{
+		cp:              cp,
+		accessFlag:      reader.readUint16(),
+		nameIndex:       reader.readUint16(),
+		descriptorIndex: reader.readUint16(),
+		attributes:      readAttributes(reader, cp),
+	}
+}
+
+func (self *MemberInfo) AccessFlag() uint16 {
+	return self.accessFlag
+}
+
+func (self *MemberInfo) Name() string {
+	return self.cp
+}
