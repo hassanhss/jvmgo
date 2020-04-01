ea585ca6c9fa9c18deed913986e89a725644b3d1
hassan <hassanbox@qq.com>
2020-04-01 22:53:18 +0800

    constant pool

--- ch03/classfile/constant_pool.go
+++ ch03/classfile/constant_pool.go
@@ -26,5 +26,18 @@ func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
 }
 
 func (self ConstantPool) getNameAndType(index uint16) (string, string) {
+	ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	name := self.getUtf8(ntInfo.nameIndex)
+	_type := self.getUtf8(ntInfo.descriptorIndex)
+	return name, _type
+}
 
+func (self ConstantPool) getClassName(index uint16) string {
+	classInfo := self.getConstantInfo(index).(*ConstantClassInfo)
+	return self.getUtf8(classInfo.nameIndex)
+}
+
+func (self ConstantPool) getUtf8(index uint16) string {
+	utf8Info := self.getConstantInfo(index).(*ConstantUtf8Info)
+	return utf8Info.str
 }
