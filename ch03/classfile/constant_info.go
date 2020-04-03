ec90c28240b4d04e08dc281e0bc396b2ad3398e1
hanshunshun <hanshunshun@pinming.cn>
2020-04-03 13:30:17 +0800

    ch03

--- ch03/classfile/constant_info.go
+++ ch03/classfile/constant_info.go
@@ -2,7 +2,7 @@ package classfile
 
 const (
 	CONSTANT_Class              = 7
-	CONSTANT_Fieldsref          = 9
+	CONSTANT_Fieldref           = 9
 	CONSTANT_Methodref          = 10
 	CONSTANT_InterfaceMethodref = 11
 	CONSTANT_String             = 8
