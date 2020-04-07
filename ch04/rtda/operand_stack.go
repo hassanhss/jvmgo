67c7b4d1f9e83c0d7c55ec5189e49f705a673212
hanshunshun <hanshunshun@pinming.cn>
2020-04-07 14:12:21 +0800

    ch04

--- ch04/rtda/operand_stack.go
+++ ch04/rtda/operand_stack.go
@@ -4,3 +4,12 @@ type OperandStack struct {
 	size  uint
 	slots []Slot
 }
+
+func newOperandStack(maxLock uint) *OperandStack {
+	if maxLock > 0 {
+		return &OperandStack{
+			slots: make([]Slot, maxLock),
+		}
+	}
+	return nil
+}
