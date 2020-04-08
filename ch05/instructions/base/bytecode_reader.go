f49e1ab92d7acc6ee4687483ff75a9062bc61234
hanshunshun <hanshunshun@pinming.cn>
2020-04-08 20:15:47 +0800

    ch05

---
+++ ch05/instructions/base/bytecode_reader.go
@@ -0,0 +1,6 @@
+package base
+
+type BytecodeReader struct {
+	code []byte
+	pc   int
+}
---
+++ ch05/instructions/base/instruction.go
@@ -0,0 +1,8 @@
+package base
+
+import "jvmgo/ch05/rtda"
+
+type Instruction interface {
+	FetchOperands(reader *BytecodeReader)
+	Execute(frame *rtda.Frame)
+}
