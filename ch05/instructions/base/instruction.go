c29f763b683e213b2b76c4b5fab79ca0b6635b5e
hassan <hassanbox@qq.com>
2020-04-08 22:24:47 +0800

    ch05

--- ch05/instructions/base/instruction.go
+++ ch05/instructions/base/instruction.go
@@ -6,3 +6,19 @@ type Instruction interface {
 	FetchOperands(reader *BytecodeReader)
 	Execute(frame *rtda.Frame)
 }
+
+type NoOperandsInstruction struct {
+	//empty
+}
+
+func (self *NoOperandsInstruction) FetchOperands(reader *BytecodeReader) {
+	//nothing to do
+}
+
+type BranchInstruction struct {
+	Offset int
+}
+
+func (self *BranchInstruction) FetchOperands(reader *BytecodeReader) {
+
+}
