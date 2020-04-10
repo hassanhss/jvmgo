d8b2d002b90575f1c5d29896bff535aad68037c5
hanshunshun <hanshunshun@pinming.cn>
2020-04-10 15:29:05 +0800

    ch05

---
+++ ch05/instructions/stores/dstore.go
@@ -0,0 +1,43 @@
+package stores
+
+import (
+	"jvmgo/ch05/instructions/base"
+	"jvmgo/ch05/rtda"
+)
+
+type DSTORE struct {
+	base.Index8Instruction
+}
+
+func _dstore(frame *rtda.Frame, index uint) {
+	val := frame.OperandStack().PopDouble()
+	frame.LocalVars().SetDouble(index, val)
+}
+
+func (self *DSTORE) Execute(frame *rtda.Frame) {
+	_dstore(frame, 0)
+}
+
+type DSTORE_1 struct {
+	base.NoOperandsInstruction
+}
+
+func (self *DSTORE_1) Execute(frame *rtda.Frame) {
+	_dstore(frame, 1)
+}
+
+type DSTORE_2 struct {
+	base.NoOperandsInstruction
+}
+
+func (self *DSTORE_2) Execute(frame *rtda.Frame) {
+	_dstore(frame, 2)
+}
+
+type DSTORE_3 struct {
+	base.NoOperandsInstruction
+}
+
+func (self *DSTORE_3) Execute(frame *rtda.Frame) {
+	_dstore(frame, 3)
+}
