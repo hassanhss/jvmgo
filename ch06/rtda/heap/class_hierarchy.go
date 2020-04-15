9a30b6d751969d102d173e04fe26ba5bc34bde2f
hassan <hassanbox@qq.com>
2020-04-15 22:27:15 +0800

    ch06

---
+++ ch06/rtda/heap/class_hierarchy.go
@@ -0,0 +1,11 @@
+package heap
+
+// self extends c
+func (self *Class) isSubClassOf(other *Class) bool {
+	for c := self.superClass; c != nil; c = c.superClass {
+		if c == other {
+			return true
+		}
+	}
+	return false
+}
