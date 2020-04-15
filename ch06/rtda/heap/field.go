57013dc91ef695f4559bdbb85058a265bd6df1e1
hassan <hassanbox@qq.com>
2020-04-15 22:35:53 +0800

    ch06

--- ch06/rtda/heap/field.go
+++ ch06/rtda/heap/field.go
@@ -24,3 +24,27 @@ func (self *Field) copyAttributes(cfField *classfile.MemberInfo) {
 		self.constValueIndex = uint(valAttr.ConstantValueIndex())
 	}
 }
+
+func (self *Field) IsVolatile() bool {
+	return 0 != self.accessFlags&ACC_VOLATILE
+}
+
+func (self *Field) IsTransient() bool {
+	return 0 != self.accessFlags&ACC_TRANSIENT
+}
+
+func (self *Field) IsEnum() bool {
+	return 0 != self.accessFlags&ACC_ENUM
+}
+
+func (self *Field) ConstValueIndex() uint {
+	return self.constValueIndex
+}
+
+func (self *Field) SlotId() uint {
+	return self.slotId
+}
+
+func (self *Field) isLongOrDouble() bool {
+	return self.descriptor == "J" || self.descriptor == "D"
+}
