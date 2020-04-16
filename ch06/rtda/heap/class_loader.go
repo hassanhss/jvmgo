808122481ee0d37fa531d9ff5150b384a4aae1f7
hanshunshun <hanshunshun@pinming.cn>
2020-04-16 16:01:25 +0800

    ch06

--- ch06/rtda/heap/class_loader.go
+++ ch06/rtda/heap/class_loader.go
@@ -13,3 +13,29 @@ type ClassLoader struct {
 	cp       *classpath.Classpath
 	classMap map[string]*Class
 }
+
+func NewClassLoader(cp *classpath.Classpath) *ClassLoader {
+	return &ClassLoader{
+		cp:       cp,
+		classMap: make(map[string]*Class),
+	}
+}
+
+func (self *ClassLoader) LoadClass(name string) *Class {
+	if class, ok := self.classMap[name]; ok {
+		return class
+	}
+	return self.loadNonArrayClass(name)
+}
+
+func (self *ClassLoader) loadNonArrayClass(name string) *Class {
+	return nil
+}
+
+func (self *ClassLoader) readClass(name string) ([]byte, classpath.Entry) {
+	data, entry, err := self.cp.ReadClass(name)
+	if err != nil {
+		panic("java.lang.ClassNotFoundException: " + name)
+	}
+	return data, entry
+}
