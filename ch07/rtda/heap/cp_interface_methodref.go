52811d00f624881533e784dc45ba69d81bec86e1
hanshunshun <hanshunshun@pinming.cn>
2020-04-26 09:40:04 +0800

    ch07 init

--- ch07/rtda/heap/cp_interface_methodref.go
+++ ch07/rtda/heap/cp_interface_methodref.go
@@ -16,7 +16,33 @@ func newInterfaceMethodRef(pool *ConstantPool, refInfo *classfile.ConstantInterf
 
 func (self *InterfaceMethodRef) ResolvedInterfaceInterfaceMethod() *Method {
 	if self.method == nil {
-		//self.method = res
+		self.resolveInterfaceMethodRef()
 	}
 	return nil
+}
+
+func (self *InterfaceMethodRef) resolveInterfaceMethodRef() {
+	d := self.pool.class
+	c := self.ResolvedClass()
+	if !c.IsInterface() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+	method := lookupInterfaceMethod(c, self.name, self.descriptor)
+	if method == nil {
+		panic("java.lang.NoSuchMethodError")
+	}
+	if !method.IsAccessibleTo(d) {
+		panic("java.lang.IllegalAccessError")
+	}
+	self.method = method
+}
+
+func lookupInterfaceMethod(iface *Class, name string, descriptor string) *Method {
+	for _, method := range iface.methods {
+		if method.name == name && method.descriptor == descriptor {
+			return method
+		}
+	}
+	return lookupMethodInInterfaces(iface.interfaces, name, descriptor)
+
 }
