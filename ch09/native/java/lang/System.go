5e4bff19593ccd06e2ffd1df31db0a1bd5d88feb
hanshunshun <hanshunshun@pinming.cn>
2020-05-07 10:48:49 +0800

    ch09

--- ch09/native/java/lang/System.go
+++ ch09/native/java/lang/System.go
@@ -10,6 +10,8 @@ func init() {
 	native.Register("java/lang/System", "arraycopy", "(Ljava/lang/Object;ILjava/lang/Object;II)V", arraycopy)
 }
 
+// public static native void arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+// (Ljava/lang/Object;ILjava/lang/Object;II)V
 func arraycopy(frame *rtda.Frame) {
 	vars := frame.LocalVars()
 	src := vars.GetRef(0)
@@ -21,24 +23,27 @@ func arraycopy(frame *rtda.Frame) {
 	if src == nil || dest == nil {
 		panic("java.lang.NullPointerException")
 	}
-
 	if !checkArrayCopy(src, dest) {
 		panic("java.lang.ArrayStoreException")
 	}
-
-	if srcPos < 0 || destPos < 0 || length < 0 || srcPos+length > src.ArrayLength() || destPos+length > dest.ArrayLength() {
+	if srcPos < 0 || destPos < 0 || length < 0 ||
+		srcPos+length > src.ArrayLength() ||
+		destPos+length > dest.ArrayLength() {
 		panic("java.lang.IndexOutOfBoundsException")
 	}
+
 	heap.ArrayCopy(src, dest, srcPos, destPos, length)
 }
 
-func checkArrayCopy(src *heap.Object, dest *heap.Object) bool {
+func checkArrayCopy(src, dest *heap.Object) bool {
 	srcClass := src.Class()
 	destClass := dest.Class()
+
 	if !srcClass.IsArray() || !destClass.IsArray() {
 		return false
 	}
-	if srcClass.ComponentClass().IsPrimitive() || destClass.ComponentClass().IsPrimitive() {
+	if srcClass.ComponentClass().IsPrimitive() ||
+		destClass.ComponentClass().IsPrimitive() {
 		return srcClass == destClass
 	}
 	return true
