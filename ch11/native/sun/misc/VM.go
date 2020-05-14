c0bfac42a8475cdb85af3aad18f959f3bec0e4f4
hanshunshun <hanshunshun@pinming.cn>
2020-05-14 20:02:13 +0800

    ch11

--- ch11/native/sun/misc/VM.go
+++ ch11/native/sun/misc/VM.go
@@ -12,17 +12,7 @@ func init() {
 // private static native void initialize();
 // ()V
 func initialize(frame *rtda.Frame) { // hack: just make VM.savedProps nonempty
-	vmClass := frame.Method().Class()
-	savedProps := vmClass.GetRefVar("savedProps", "Ljava/util/Properties;")
-	key := heap.JString(vmClass.Loader(), "foo")
-	val := heap.JString(vmClass.Loader(), "bar")
-
-	frame.OperandStack().PushRef(savedProps)
-	frame.OperandStack().PushRef(key)
-	frame.OperandStack().PushRef(val)
-
-	propsClass := vmClass.Loader().LoadClass("java/util/Properties")
-	setPropMethod := propsClass.GetInstanceMethod("setProperty",
-		"(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/Object;")
-	base.InvokeMethod(frame, setPropMethod)
+	classLoader := frame.Method().Class().Loader()
+	jISysClass := classLoader.LoadClass("java/lang/System")
+	//initSysClass := jISysClass.Get
 }
